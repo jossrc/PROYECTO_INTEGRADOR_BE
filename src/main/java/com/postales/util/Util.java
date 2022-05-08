6ad96ac4741c2ae2ca416a97d4d21fe763baf570
jossrc <kuro.rc10@gmail.com>
2022-05-07 19:22:11 -0500

    util fix return

--- src/main/java/com/postales/util/Util.java
+++ src/main/java/com/postales/util/Util.java
@@ -4,7 +4,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Util {
 
-    public static double generarEnteroAleatorio(int minimo, int maximo) {
+    /**
+     * Genera un número entero aleatorio
+     * @param minimo Mínimo valor entero
+     * @param maximo Máximo valor entero
+     * @return Número entero aleatorio
+     */
+    public static int generarEnteroAleatorio(int minimo, int maximo) {
         return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
     }
 
