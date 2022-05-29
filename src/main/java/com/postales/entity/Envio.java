6f0dcf59bc49d210b63840edc544c85cd8e2aae3
FrancoHub25 <francoretamozo25@gmail.com>
2022-05-29 17:58:52 -0500

    modelo envio actualizado

--- src/main/java/com/postales/entity/Envio.java
+++ src/main/java/com/postales/entity/Envio.java
@@ -4,14 +4,18 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -24,12 +28,25 @@ public class Envio implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idEnv;
-	private String descripcion;
+	private int idEnvio;
+	private String adjunto;
 	
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	@Temporal(TemporalType.DATE)
-	private Date fechaSolicitud;
+	private Date fechaInicio;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Temporal(TemporalType.DATE)
+	private Date fechaEntrega;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Temporal(TemporalType.DATE)
+	private Date fechaCreacion;
+	
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idUsuario")
+	private Usuario usuario;
 	
 	private String estado;
 	
